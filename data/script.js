e05aa423203306280d6f4ddb2fa6d8e56cf1ce5f
DanyZugz <danielaguilard99@gmail.com>
2023-09-20 14:11:44 -0400

    changes

--- data/script.js
+++ data/script.js
@@ -1,12 +1,31 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.getElementById('chat-messages');
 
+const socket = new WebSocket('ws://' + location.hostname + ':80/ws');
+socket.onopen = (oevent) => {
+  socket.onmessage =  (event) => {
+    const messageincoming = event.data;
+    const messageElem = document.createElement('p');
+    messageElem.textContent = messageincoming;
 
-chatForm.addEventListener('submit', function(event) {
+    messageElem.classList.add('message');
+    messageElem.classList.add('message-incoming');
+
+    chatMessages.appendChild(messageElem);
+
+    // Scroll chat window to the bottom
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  };
+}
+
+//listener del formulario
+chatForm.addEventListener('submit', function (event) {
   event.preventDefault();
 
   const message = chatForm.elements['message'].value;
+  socket.send(message);
   chatForm.elements['message'].value = '';
+
   const newMessage = document.createElement('p');
   newMessage.textContent = message;
 
@@ -16,4 +35,8 @@ chatForm.addEventListener('submit', function(event) {
 
 
   chatMessages.appendChild(newMessage);
+
+
+  // Scroll chat window to the bottom
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,43 @@
-#include <WiFi.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include "web_page.h"
 #include "wifi_config.h"
+#include "LoRaWan_APP.h"
+#include "SPIFFS.h"
+
+#define RF_FREQUENCY                                865000000 // Hz
+#define TX_OUTPUT_POWER                             5        // dBm
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
+#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5,
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+#define BUFFER_SIZE                                 30 // Define the payload size here
+
+char txpacket[BUFFER_SIZE];
+char rxpacket[BUFFER_SIZE];
+
+static RadioEvents_t RadioEvents;
+
+void OnTxDone(void);
+
+void OnTxTimeout(void);
+
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr);
+
+
+typedef enum {
+    LOWPOWER,
+    STATE_RX,
+    STATE_TX
+} States_t;
+
+States_t state;
+int16_t Rssi, rxSize;
+char my_data[255];
 
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
@@ -26,9 +61,8 @@ void onEvent(AsyncWebSocket *myserver, AsyncWebSocketClient *client, AwsEventTyp
 //                strncpy(data, (const char *) midata, len);
 //                Serial.println(data);
 //                free(data);
-                  Serial.println(String((const char *) midata));
-
-
+                Serial.println(String((const char *) midata));
+                strncpy(my_data, reinterpret_cast<const char *>(midata), len + 1);
                 // Additional processing of the message
                 // ...
             }
@@ -45,25 +79,97 @@ void setup() {
     // put your setup code here, to run once:
     Serial.begin(9600);
 
+    // Initialize SPIFFS
+    if (!SPIFFS.begin(true)) {
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+    }
+
     setWifi();
 
     ws.onEvent(onEvent);
     server.addHandler(&ws);
 
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-        request->send(200, "text/html", html);
+        request->send(SPIFFS, "/index.html", "text/html");
     });
+
+    server.serveStatic("/", SPIFFS, "/");
 
     // Start the server
     server.begin();
+
+    Mcu.begin();
+    Rssi = 0;
+
+    RadioEvents.TxDone = OnTxDone;
+    RadioEvents.TxTimeout = OnTxTimeout;
+    RadioEvents.RxDone = OnRxDone;
+
+    Radio.Init(&RadioEvents);
+    Radio.SetChannel(RF_FREQUENCY);
+    Radio.SetTxConfig(MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                      LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                      LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                      true, false, 0, LORA_IQ_INVERSION_ON, 3000);
+
+    Radio.SetRxConfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                      LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                      LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                      0, true, false, 0, LORA_IQ_INVERSION_ON, true);
+    state = STATE_TX;
 }
+
 
 void loop() {
     ws.cleanupClients();
 
-    if (Serial.available()) {
-        String mydata = Serial.readStringUntil('\n');
-        // TODO: Process the data
-        ws.textAll(mydata);
+    switch (state) {
+        case STATE_TX:
+            delay(1000);
+            Serial.printf("data to be sent: %s", my_data);
+            sprintf(txpacket, "%s", my_data);
+            Serial.printf("\r\nsending packet \"%s\" , length %d\r\n", txpacket, strlen(txpacket));
+            Radio.Send((uint8_t *) txpacket, strlen(txpacket));
+            strcpy(my_data, "");
+            state = LOWPOWER;
+            break;
+        case STATE_RX:
+            Serial.println("into RX mode");
+            Radio.Rx(0);
+            state = LOWPOWER;
+            break;
+        case LOWPOWER:
+            Radio.IrqProcess();
+            break;
+        default:
+            break;
     }
+    delay(10);
+}
+
+void OnTxDone(void) {
+    Serial.print("TX done......");
+    state = STATE_RX;
+}
+
+void OnTxTimeout(void) {
+    Radio.Sleep();
+    Serial.print("TX Timeout......");
+    state = STATE_TX;
+}
+
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr) {
+    Rssi = rssi;
+    rxSize = size;
+    memcpy(rxpacket, payload, size);
+    rxpacket[size] = '\0';
+    Radio.Sleep();
+
+    Serial.printf("\r\nreceived packet \"%s\" with Rssi %d , length %d\r\n", rxpacket, Rssi, rxSize);
+    Serial.println("wait to send next packet");
+    if (rxSize != 0) {
+        ws.textAll(String(rxpacket));
+    }
+    state = STATE_TX;
 }
